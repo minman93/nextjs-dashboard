3d0fdc0d7babcfdc216ccdf2807c719c8df81609
minman93 <matt_j_inman@hotmail.co.uk>
2024-01-15 20:00:05 +0000

    string fixed

--- app/page.tsx
+++ app/page.tsx
@@ -42,6 +42,7 @@ export default function Page() {
             width={560}
             height={620}
             className="hidden md:block"
+            alt="Screenshots of the dashboard project showing mobile"
           />
         </div>
       </div>
